5b8eae660a2869a99adeb9cd62295af34da45fc9
maxwell-clark <Maxwell6636@gmail.com>
2024-04-21 12:02:16 -0500

    first commit

--- components/Header.vue
+++ components/Header.vue
@@ -3,20 +3,22 @@ const nuxtApp = useNuxtApp()
 const { activeHeadings, updateHeadings } = useScrollspy()
 
 const links = computed(() => [{
-  label: 'Features',
+  label: 'Specialties',
   to: '#features',
   icon: 'i-heroicons-cube-transparent',
-  active: activeHeadings.value.includes('features') && !activeHeadings.value.includes('pricing')
-}, {
-  label: 'Pricing',
-  to: '#pricing',
+  active: activeHeadings.value.includes('features') && !activeHeadings.value.includes('photos')
+},
+  {
+  label: 'Gallery',
+  to: '#photos',
   icon: 'i-heroicons-credit-card',
-  active: activeHeadings.value.includes('pricing') && !activeHeadings.value.includes('testimonials')
-}, {
+  active: activeHeadings.value.includes('photos') && !activeHeadings.value.includes('testimonials')
+},
+  {
   label: 'Testimonials',
   to: '#testimonials',
   icon: 'i-heroicons-academic-cap',
-  active: activeHeadings.value.includes('testimonials')
+  active: activeHeadings.value.includes('testimonials') && !activeHeadings.value.includes('photos')
 }, {
   label: 'FAQ',
   to: '#faq',
@@ -27,7 +29,7 @@ const links = computed(() => [{
 nuxtApp.hooks.hookOnce('page:finish', () => {
   updateHeadings([
     document.querySelector('#features'),
-    document.querySelector('#pricing'),
+    document.querySelector('#photos'),
     document.querySelector('#testimonials'),
     document.querySelector('#faq')
   ])
@@ -37,20 +39,40 @@ nuxtApp.hooks.hookOnce('page:finish', () => {
 <template>
   <UHeader :links="links">
     <template #logo>
-      Nuxt UI Pro <UBadge label="Landing" variant="subtle" class="mb-0.5" />
+      <img class="header-image" src="~/assets/img/dc_homes_logo_small.png" />
     </template>
 
     <template #right>
-      <UButton label="Sign in" color="white" variant="ghost" trailing-icon="i-heroicons-arrow-right-20-solid" class="hidden lg:flex" />
+      <UButton label="Contact Us" color="white" to="#contact" variant="ghost" trailing-icon="i-heroicons-arrow-right-20-solid" class="hidden lg:flex" />
     </template>
 
     <template #panel>
-      <UAsideLinks :links="links" />
+      <div class="bg-white dark:bg-gray-950 dark:bg-opacity-90 p-3">
+        <UAsideLinks :links="links" />
 
-      <UDivider class="my-6" />
+        <UDivider class="my-6" />
 
-      <UButton label="Sign in" color="white" block class="mb-3" />
-      <UButton label="Get started" block />
+        <UButton label="Contact Us" color="white" block class="mb-3" />
+      </div>
     </template>
   </UHeader>
 </template>
+
+<style>
+.header-image {
+  width: 100%; /* Full width on smaller screens */
+  max-width: 400px; /* Maximum width */
+  height: auto; /* Maintain aspect ratio */
+}
+
+@media (min-width: 768px) {
+  .header-image {
+    width: 50%; /* Half width on larger screens */
+  }
+}
+@media (max-width: 768px) {
+  .header-image {
+    width: 45%; /* Full width on smaller screens */
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,34 @@ useSeoMeta({
   description: page.value.description,
   ogDescription: page.value.description
 })
+
+import { object, string, type InferType } from 'yup'
+import type { FormSubmitEvent } from '#ui/types'
+
+const schema = object({
+  email: string().email('Invalid email').required('Required'),
+  name: string().required('Required'),
+  message: string().required('Required')
+})
+
+type Schema = InferType<typeof schema>
+
+const state = reactive({
+  email: undefined,
+  name: undefined,
+  message: undefined
+})
+
+async function onSubmit (event: FormSubmitEvent<Schema>) {
+  // Do something with event.data
+  console.log(event.data)
+}
+
 </script>
 
 <template>
   <div>
+
     <ULandingHero :title="page.hero.title" :description="page.hero.description" :links="page.hero.links">
       <template #headline>
         <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
@@ -24,35 +48,31 @@ useSeoMeta({
         </UBadge>
       </template>
 
-      <Placeholder />
+<!--      <Placeholder />-->
 
-      <ULandingLogos :title="page.logos.title" align="center">
-        <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon" class="w-12 h-12 lg:w-16 lg:h-16 flex-shrink-0 text-gray-900 dark:text-white" />
-      </ULandingLogos>
+<!--      <ULandingLogos :title="page.logos.title" align="center">-->
+<!--        <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon" class="w-12 h-12 lg:w-16 lg:h-16 flex-shrink-0 text-gray-900 dark:text-white" />-->
+<!--      </ULandingLogos>-->
     </ULandingHero>
 
-    <ULandingSection :title="page.features.title" :description="page.features.description" :headline="page.features.headline">
-      <UPageGrid id="features" class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
+    <ULandingSection id="features" :title="page.features.title" :description="page.features.description" :headline="page.features.headline">
+      <UPageGrid class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
         <ULandingCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" />
       </UPageGrid>
     </ULandingSection>
 
-    <ULandingSection :title="page.pricing.title" :description="page.pricing.description" :headline="page.pricing.headline">
-      <UPricingGrid id="pricing" compact class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
-        <UPricingCard v-for="(plan, index) in page.pricing.plans" :key="index" v-bind="plan" />
-      </UPricingGrid>
+    <ULandingSection id="photos" :title="page.gallery.title" :description="page.gallery.description" :headline="page.gallery.headline">
+      <UCarousel v-slot="{ item }" :items="page.gallery.pictures" :ui="{ item: 'basis-full' }" class="rounded-lg overflow-hidden" arrows>
+        <img :src="item.url" class="w-full" draggable="false" :alt="item.alt">
+      </UCarousel>
     </ULandingSection>
 
-    <ULandingSection :headline="page.testimonials.headline" :title="page.testimonials.title" :description="page.testimonials.description">
-      <UPageColumns id="testimonials" class="xl:columns-4 scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
+    <ULandingSection id="testimonials" :headline="page.testimonials.headline" :title="page.testimonials.title" :description="page.testimonials.description">
+      <UPageColumns class="xl:columns-4 scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
         <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
           <ULandingTestimonial v-bind="testimonial" />
         </div>
-      </UPageColumns>
-    </ULandingSection>
-
-    <ULandingSection class="bg-primary-50 dark:bg-primary-400 dark:bg-opacity-10">
-      <ULandingCTA v-bind="page.cta" :card="false" />
+      </UPageColumns >
     </ULandingSection>
 
     <ULandingSection id="faq" :title="page.faq.title" :description="page.faq.description" class="scroll-mt-[var(--header-height)]">
@@ -69,6 +89,26 @@ useSeoMeta({
         }"
         class="max-w-4xl mx-auto"
       />
+    </ULandingSection>
+
+    <ULandingSection id="contact" :title="page.contact.title" :description="page.contact.description" class=" flex flex-col w-full justify-center items-center bg-gray-50 dark:bg-gray-400 dark:bg-opacity-10">
+      <UForm :schema="schema" :state="state" class="space-y-4 w-full flex flex-col justify-center" @submit="onSubmit">
+        <UFormGroup label="Your Email" name="email">
+          <UInput v-model="state.email" />
+        </UFormGroup>
+
+        <UFormGroup label="Your Name" name="name">
+          <UInput v-model="state.name" />
+        </UFormGroup>
+
+        <UFormGroup label="Message" name="message">
+          <UTextarea v-model="state.message" />
+        </UFormGroup>
+
+        <UButton type="submit">
+          Submit
+        </UButton>
+      </UForm>
     </ULandingSection>
   </div>
 </template>
